35beeee572bd617e20c8ea1a769a408765cf5757
Nossi201 <53948987+Nossi201@users.noreply.github.com>
2024-04-18 18:47:05 +0200

    Aktualizacja plików lokalnych na zdalnym repozytorium

--- tmp.py
+++
@@ -1,9 +0,0 @@
-with open("Zeszyt1.txt", "r", encoding="utf-8") as file:
-    lines = file.readlines()
-
-with open("Zeszyt_modified.txt", "w", encoding="utf-8") as modified_file:
-    for line in lines:
-        line = line.rstrip("\n")  # Usuwamy znak końca linii
-        line = line.replace("'", "\\'")  # Dodajemy znak ucieczki przed każdym apostrofem
-        modified_line = line.replace("\t", ":'") + "',\n"  # Dokonujemy zamiany i dodajemy przecinek przed końcem linii
-        modified_file.write(modified_line)
