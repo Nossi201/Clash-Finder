c23ebda87fbb9b1a384c4d78aac655d839bcfb31
Nossi201 <53948987+Nossi201@users.noreply.github.com>
2025-05-19 23:19:34 +0200

    delete ussles import

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 # Configuration file for API keys and other settings
 
-RIOT_API_KEY = ""
+RIOT_API_KEY = "RGAPI-ca3add65-ecc1-4d4e-9843-bc6e017571d4"
 
 
-FLASK_SECRET_KEY = ""
+FLASK_SECRET_KEY = "c7f8e4b3a9d2f1e6b5a4c3d2e1f0a9b8c7d6e5f4a3b2c1d0e9f8a7b6c5d4e3f2"
